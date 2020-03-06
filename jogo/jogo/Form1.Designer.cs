11cb100d0804d6f295eb1fd1fd82f2a18343122f
GUSTAVO YAMAUCHI SOUZA REIS <gustavo.ysreis@senacsp.edu.br>
2020-03-06 20:51:12 -0300

    Primeiro commit

---
+++ jogo/jogo/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace jogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListarPartidas = new System.Windows.Forms.Button();
+            this.txtPartidas = new System.Windows.Forms.TextBox();
+            this.cboPartidas = new System.Windows.Forms.ComboBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.txtIdPartida = new System.Windows.Forms.TextBox();
+            this.txtNomePartida = new System.Windows.Forms.TextBox();
+            this.lblIdPartida = new System.Windows.Forms.Label();
+            this.lblNomePartida = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnListarPartidas
+            // 
+            this.btnListarPartidas.Location = new System.Drawing.Point(6, 29);
+            this.btnListarPartidas.Name = "btnListarPartidas";
+            this.btnListarPartidas.Size = new System.Drawing.Size(75, 23);
+            this.btnListarPartidas.TabIndex = 0;
+            this.btnListarPartidas.Text = "Listar";
+            this.btnListarPartidas.UseVisualStyleBackColor = true;
+            this.btnListarPartidas.Click += new System.EventHandler(this.btnListarPartidas_Click);
+            // 
+            // txtPartidas
+            // 
+            this.txtPartidas.Enabled = false;
+            this.txtPartidas.Location = new System.Drawing.Point(101, 31);
+            this.txtPartidas.Multiline = true;
+            this.txtPartidas.Name = "txtPartidas";
+            this.txtPartidas.Size = new System.Drawing.Size(149, 114);
+            this.txtPartidas.TabIndex = 1;
+            this.txtPartidas.TextChanged += new System.EventHandler(this.txtPartidas_TextChanged);
+            // 
+            // cboPartidas
+            // 
+            this.cboPartidas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPartidas.FormattingEnabled = true;
+            this.cboPartidas.Location = new System.Drawing.Point(162, 24);
+            this.cboPartidas.Name = "cboPartidas";
+            this.cboPartidas.Size = new System.Drawing.Size(149, 21);
+            this.cboPartidas.TabIndex = 2;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(236, 104);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 3;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // txtIdPartida
+            // 
+            this.txtIdPartida.Location = new System.Drawing.Point(67, 67);
+            this.txtIdPartida.Name = "txtIdPartida";
+            this.txtIdPartida.Size = new System.Drawing.Size(32, 20);
+            this.txtIdPartida.TabIndex = 4;
+            // 
+            // txtNomePartida
+            // 
+            this.txtNomePartida.Location = new System.Drawing.Point(211, 67);
+            this.txtNomePartida.Name = "txtNomePartida";
+            this.txtNomePartida.Size = new System.Drawing.Size(100, 20);
+            this.txtNomePartida.TabIndex = 5;
+            // 
+            // lblIdPartida
+            // 
+            this.lblIdPartida.AutoSize = true;
+            this.lblIdPartida.Location = new System.Drawing.Point(30, 70);
+            this.lblIdPartida.Name = "lblIdPartida";
+            this.lblIdPartida.Size = new System.Drawing.Size(16, 13);
+            this.lblIdPartida.TabIndex = 6;
+            this.lblIdPartida.Text = "Id";
+            // 
+            // lblNomePartida
+            // 
+            this.lblNomePartida.AutoSize = true;
+            this.lblNomePartida.Location = new System.Drawing.Point(165, 70);
+            this.lblNomePartida.Name = "lblNomePartida";
+            this.lblNomePartida.Size = new System.Drawing.Size(35, 13);
+            this.lblNomePartida.TabIndex = 7;
+            this.lblNomePartida.Text = "Nome";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPartidas);
+            this.groupBox1.Controls.Add(this.btnListarPartidas);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 162);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Partidas";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cboPartidas);
+            this.groupBox2.Controls.Add(this.txtIdPartida);
+            this.groupBox2.Controls.Add(this.btnEntrar);
+            this.groupBox2.Controls.Add(this.lblNomePartida);
+            this.groupBox2.Controls.Add(this.lblIdPartida);
+            this.groupBox2.Controls.Add(this.txtNomePartida);
+            this.groupBox2.Location = new System.Drawing.Point(12, 198);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(366, 133);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Entrar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Partida";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListarPartidas;
+        private System.Windows.Forms.TextBox txtPartidas;
+        private System.Windows.Forms.ComboBox cboPartidas;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.TextBox txtIdPartida;
+        private System.Windows.Forms.TextBox txtNomePartida;
+        private System.Windows.Forms.Label lblIdPartida;
+        private System.Windows.Forms.Label lblNomePartida;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ jogo/jogo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AzulServer;
+
+namespace jogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListarPartidas_Click(object sender, EventArgs e)
+        {
+            string ret = Jogo.ListarPartidas("T");
+
+            ret = ret.Replace("\r", "");
+
+            string[] partidas = ret.Split('\n');
+
+            string[] itens = partidas[0].Split(',');
+
+            foreach (string partida in partidas)
+            {
+                cboPartidas.Items.Add(partida);
+            }
+
+            txtPartidas.Text = itens[1];
+        }
+
+        private void txtPartidas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string partida = cboPartidas.SelectedItem.ToString();
+
+            string[] itens = partida.Split(',');
+            
+            txtIdPartida.Text = itens[0];
+            txtNomePartida.Text = itens[1];
+        }
+    }
+}
